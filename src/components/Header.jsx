2e603bea6184b104b3a7c6087c78b78987d42139
ThivinaSam <152647603+ThivinaSam@users.noreply.github.com>
2024-08-18 21:14:06 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Header>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Fresh</span>
+            <span className='text-slate-700'>Colombo</span>
+        </h1>
+    </Header>
+  )
+}
